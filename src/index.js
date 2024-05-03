32be34da05a93ebf9d2e8948790ecfe872fdc2fe
hhgomez <hhgomez@unipacifico.edu.co>
2024-05-03 07:59:51 -0500

    agregando cors

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ app.listen(3000, () => {
     console.log(`listening on port ${app.get('port')}`);
 });
 
+const cors = require('cors');
+app.use(cors());
+
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(require('./routes/rutas'));
